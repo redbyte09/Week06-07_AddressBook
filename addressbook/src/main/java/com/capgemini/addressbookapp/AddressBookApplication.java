1a963358cb0009ab9ea510ec0953e7ecbfdb052a
redbyte09 <sakshisharma4234@gmail.com>
2025-03-01 11:02:08 +0530

    [Sakshi] Add . Create a Rest Controller to demonstrate the various HTTP Methods

--- addressbook/src/main/java/com/capgemini/addressbookapp/AddressBookApplication.java
+++ addressbook/src/main/java/com/capgemini/addressbookapp/AddressBookApplication.java
@@ -1,4 +1,4 @@
-package com.capgemini.addressbook;
+package com.capgemini.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- addressbook/src/test/java/com/capgemini/addressbookapp/AddressBookApplicationTests.java
+++ addressbook/src/test/java/com/capgemini/addressbookapp/AddressBookApplicationTests.java
@@ -1,4 +1,4 @@
-package com.capgemini.addressbook;
+package com.capgemini.addressbookapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
